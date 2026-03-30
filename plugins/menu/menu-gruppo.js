68ac9d9f7a23c02d79e3e2c540460d527bb4406c
tiktoksixseven676767-del <tiktoksixseven676767@gmail.com>
2026-03-30 21:10:24 +0200

    Update menu-gruppo.js

--- plugins/menu/menu-gruppo.js
+++ plugins/menu/menu-gruppo.js
@@ -78,7 +78,7 @@ function generateMenuText(prefix, userId, groupId) {
 🎥 *.playlist*
 🎥 *.ytsearch*
 🌐 *.tts*
-🌐 *.GitHubsearch*
+👾 *.githubsearch*
 🔊 *.tomp3* (${global.t('videoCommand', userId, groupId)})`),
         createSection(global.t('infoUtilitySection', userId, groupId), `
 🌍 *.meteo* (${global.t('cityCommand', userId, groupId)})
