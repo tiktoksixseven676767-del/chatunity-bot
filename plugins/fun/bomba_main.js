44341ae2af738fef6c32f75f6fc4671c7564c1b6
tiktoksixseven676767-del <tiktoksixseven676767@gmail.com>
2026-04-02 01:33:07 +0200

    Update bomba_main.js

--- plugins/fun/bomba_main.js
+++ plugins/fun/bomba_main.js
@@ -6,10 +6,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
     if (global.bombaSessions[chatId]) return m.reply("💣 C'è già una bomba attiva in questo gruppo!");
 
-    // Configurazione bomba
     let lettere = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V'];
     let letteraScelta = lettere[Math.floor(Math.random() * lettere.length)];
-    let durata = Math.floor(Math.random() * (45 - 20 + 1)) + 20; // Tra 20 e 45 secondi
+    let durata = Math.floor(Math.random() * (40 - 20 + 1)) + 20; 
 
     global.bombaSessions[chatId] = {
         lettera: letteraScelta,
@@ -19,23 +18,23 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         lastMsg: null
     };
 
+    // URL GIF Alternativo (più stabile per i bot)
     let msg = await conn.sendMessage(chatId, {
-        video: { url: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJueGZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3JmVwPXYxX2ludGVybmFsX2dpZl9ieV9iZCZjdD1n/f6V2vT8DhsTjW/giphy.gif' },
+        video: { url: 'https://qu.ax/ZloG.mp4' }, 
         gifPlayback: true,
-        caption: `💣 *PASSA LA BOMBA!* 💣\n\nLa bomba è in mano a @${senderId.split('@')[0]}\n\n✨ Sfida: Scrivi una parola che inizia con la lettera: *${letteraScelta}*\n\n⏳ Sbrigati! La miccia è corta!`,
+        caption: `💣 *PASSA LA BOMBA!* 💣\n\nLa bomba è in mano a @${senderId.split('@')[0]}\n\n✨ Sfida: Scrivi una parola che inizia con la lettera: *${letteraScelta}*\n\n⏳ Sbrigati! Hai pochissimi secondi!`,
         mentions: [senderId]
     }, { quoted: m });
 
     global.bombaSessions[chatId].lastMsg = msg.key.id;
 
-    // Timer per l'esplosione
     setTimeout(async () => {
         if (global.bombaSessions[chatId]) {
             let finale = global.bombaSessions[chatId];
             await conn.sendMessage(chatId, {
-                video: { url: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJueGZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3bmZ3JmVwPXYxX2ludGVybmFsX2dpZl9ieV9iZCZjdD1n/f6V2vT8DhsTjW/giphy.gif' }, // Puoi metterne una di esplosione qui
+                video: { url: 'https://qu.ax/SMap.mp4' }, 
                 gifPlayback: true,
-                caption: `💥 *BOOOOM!* 💥\n\nLa bomba è esplosa in mano a @${finale.possessore.split('@')[0]}!\n💀 HAI PERSO!`,
+                caption: `💥 *BOOOOM!* 💥\n\nLa bomba è esplosa in mano a @${finale.possessore.split('@')[0]}!\n💀 Sei stato eliminato!`,
                 mentions: [finale.possessore]
             });
             delete global.bombaSessions[chatId];
