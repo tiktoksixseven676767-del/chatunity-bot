9cfaf035da758f8a908b36694bbb97c7e5494e76
tiktoksixseven676767-del <tiktoksixseven676767@gmail.com>
2026-04-13 00:25:02 +0200

    Update slot.js

--- plugins/fun/slot.js
+++ plugins/fun/slot.js
@@ -43,7 +43,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         user.limit = (user.limit || 0) - bet;
         user.exp = Math.max(0, (user.exp || 0) - xpLoss);
         resultMsg = global.t('slotLose', userId, groupId, { uc: bet, xp: xpLoss });
-        gifFile = '../../media/perdita.gif';
     }
 
     resultMsg += global.t('slotBalance', userId, groupId, {
